036a2ba3abaf69aef8c506352b3f1cb709dd50ec
ralphandrade <ralphandrade06@gmail.com>
2024-05-21 21:50:11 -0300

    v_1.0

---
+++ main.py
@@ -0,0 +1,32 @@
+while True:
+    # usuario informa os numeros
+    x = str(input('informe o primeiro número: ')),replace(',', '.')
+    y =str(input('informe o segundo núemro: ')),replace(',','.')
+
+    # converte para numeros decimais
+    x = float(x)
+    y = float(y)
+
+    print('operações disponíveis:\n')
+    print('"+" para somar. ')
+    print('"-" para subtrair. ')
+    print('"*" para multiplcar. ')
+    print('"/" para dividir. ')
+    print('"%" para encontrar o resto da divisão. ')
+    
+    op = input('Operação desejada: ')
+
+    match op:
+        case'+':
+            print(f'A soma é: {x + y}.')
+        case'-':
+            print(f'A subtração é: {x - y}.')
+        case'*':
+            print(f'A multiplicação é: {x * y}.')
+        case'/':
+            print(f'A divisão é: {x / y}.')
+        case'%':
+            print(f'O resto da divisão é: {x % y}.')
+        case _:
+            print('operação inválida')
+            continue
